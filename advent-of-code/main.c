729228a0b82db1e9e6acda0de2bc3649486565cc
nicole <ramosmikaelanicole@gmail.com>
2024-08-10 21:41:59 +0800

    Initial Commit

---
+++ advent-of-code/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  advent-of-code
+//
+//  Created by Mikaela Nicole Ramos on 8/10/24.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
